0909077e9a7f9225641a7abe881f9eebbe46ded4
roshankahaneDSAI <roshankahane09@gmail.com>
2025-11-26 13:04:55 +0530

    Data validation step created and tested via training pipeline

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-
-from src.components.data_ingestion import DataIngestion
-from src.entity.config_entity import DataIngestionConfig
-from src.entity.config_entity import(
-    TrainingPipelineConfig,)
-from src.constant import training_pipeline
-from src.entity.artifact_entity import DataIngestionArtifact
 from src.pipelines.training_pipeline import TrainingPipeline
 
 if __name__ == "__main__":
--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -5,14 +5,17 @@ from src.exception.exception import CustomException
 from src.logging.logger import logging
 
 from src.components.data_ingestion import DataIngestion
+from src.components.data_validation import DataValidation
 
 from src.entity.config_entity import(
     TrainingPipelineConfig,
     DataIngestionConfig,
+    DataValidationConfig,
 )
 
 from src.entity.artifact_entity import (
     DataIngestionArtifact,
+    DataValidationArtifact,
 )
 
 # from src.constant.training_pipeline import TRAINING_BUCKET_NAME
@@ -35,12 +38,22 @@ class TrainingPipeline:
         
         except Exception as e:
             raise CustomException(e,sys)
-        
+
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
+        try:
+            data_validation_config=DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation=DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=data_validation_config)
+            logging.info("Initiate the data Validation")
+            data_validation_artifact=data_validation.initiate_data_validation()
+            return data_validation_artifact
+        except Exception as e:
+            raise CustomException(e,sys)        
     
     
     def run_pipeline(self):
         try:
             data_ingestion_artifact=self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             pass    
             # return model_trainer_artifact
         except Exception as e:
